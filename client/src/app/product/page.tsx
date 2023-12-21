46e5456a41ca63d6596af0bfc3a7c6d71e760f2c
Ahmed Mess <ahmedmessaoud663@gmail.com>
2023-12-21 13:07:16 +0100

    tanstack front folder

--- client/src/app/product/page.tsx
+++ client/src/app/product/page.tsx
@@ -39,21 +39,28 @@ interface Product {
     console.log("Products:", products);
   
     return (
-      <main className='mt-4 flex min-h-screen flex-col items-center justify-between p-24'> 
-        <h1 className='text-xl'>Products</h1>
-        <div>
-          {Array.isArray(products) && products.map((product: Product) => (
-            <ul key={product.id}>
-              <li>Name: {product.name}</li>
-              <li>Price: {product.price}</li>
-              <li>Pictures: {product.pictures[0]}</li> {/* Assuming pictures is an array */}
-              <li>Category: {product.category}</li>
-              <li>Collection: {product.collection}</li>
-            </ul>
-          ))}
+      <div className="container mx-auto p-4 lg:h-screen flex items-center justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-100">
+          {Array.isArray(products) &&
+            products.map((product: Product, index: number) => (
+              <div key={index} className="w-full max-w-sm mx-auto relative shadow-md rounded-lg cursor-pointer">
+                <img
+                  src={product.pictures[0]}
+                  alt={product.name}
+                  className="w-full h-64 object-cover rounded-lg"
+                />
+                <div className="absolute bottom-0 left-0 right-0 h-40 bg-black bg-opacity-10 backdrop-blur text-white p-4 rounded-b-lg">
+                  <h1 className="text-2xl font-semibold">{product.name}</h1>
+                  <p className="mt-2">{product.category}</p>
+                </div>
+              </div>
+            ))}
         </div>
-      </main>
+      </div>
     );
   };
   
+  
 export default Page;
+
+// https://www.youngla.com/cdn/shop/products/YLA10.29.22Johnny-98_800x.jpg?v=1666901743
