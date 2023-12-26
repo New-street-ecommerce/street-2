6ccbee076d51294b95377ee190b24535f24bc408
Ahmed Mess <ahmedmessaoud663@gmail.com>
2023-12-26 09:26:46 +0100

    design about

--- client/src/app/NavBar/page.tsx
+++ client/src/app/NavBar/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
-import { FaSearch, FaHome, FaBook, FaChartBar } from "react-icons/fa";
-import ExploreDropDown from "./ExploreDropDown";
+import { FaSearch, FaHome, FaBook, FaChartBar, } from "react-icons/fa";
+// import ExploreDropDown from "./ExploreDropDown";
 
 const NavBar = () => {
   return (
