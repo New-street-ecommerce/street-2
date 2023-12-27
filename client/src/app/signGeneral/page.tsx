6d91dfbc3cc7618706f1df319791ac9220a9c407
Ahmed Mess <ahmedmessaoud663@gmail.com>
2023-12-27 08:53:02 +0100

    last push

--- client/src/app/signGeneral/page.tsx
+++ client/src/app/signGeneral/page.tsx
@@ -1,7 +1,7 @@
-"use client"
-import Image from "next/image"
-import Link from "next/link"
-import art from "../../../assets/art.jpg"
+// "use client"
+// import Image from "next/image"
+// import Link from "next/link"
+// import art from "../../../assets/art.jpg"
 
 // const signGeneral = () => {
 //     return (
